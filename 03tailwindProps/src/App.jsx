0d174bbbe7a5194723d77ed4fa7e394db34a3b64
Adit <arnavle1998@gmail.com>
2023-10-03 19:45:43 +0530

    03tailwindProps tailwind

---
+++ 03tailwindProps/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let deer =
+    "https://images.pexels.com/photos/18248176/pexels-photo-18248176/free-photo-of-roe-among-grass.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+  return (
+    <>
+      <h1 className="bg-blue-400 p-5 rounded-xl text-black">Tailwind Test</h1>
+      <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+        <img
+          class="w-24 h-24 rounded-full mx-auto"
+          src={deer}
+          alt=""
+          width="384"
+          height="512"
+        />
+        <div class="pt-6 space-y-4">
+          <blockquote>
+            <p class="text-lg font-medium">
+              “Tailwind CSS is the only framework that I've seen scale on large
+              teams. It’s easy to customize, adapts to any design, and the build
+              size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption class="font-medium">
+            <div clas>Sarah Dayan</div>
+            <div>Staff Engineer, Algolia</div>
+          </figcaption>
+        </div>
+      </figure>
+    </>
+  );
+}
+
+export default App;
